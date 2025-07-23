4dfc07b11fbc50a30de80531fcbb6989d8c9e7de
TobogganDev <doretg.t@gmail.com>
2025-07-23 10:19:13 +0200

    fix cloudfront

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
+# Bucket S3
 resource "aws_s3_bucket" "main" {
   bucket = "${var.bucket_name}-${random_integer.random.result}"
-
-  tags = var.tags
+  tags   = var.tags
 }
 
 resource "aws_s3_bucket_website_configuration" "main" {
@@ -32,33 +32,65 @@ resource "aws_s3_bucket_public_access_block" "main" {
   restrict_public_buckets = false
 }
 
-# Origin Access Control (remplace OAI)
-resource "aws_cloudfront_origin_access_control" "main" {
-  name                              = "${var.bucket_name}-oac"
-  description                       = "OAC for ${var.bucket_name}"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+resource "aws_s3_bucket_policy" "allow_content_public" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.allow_content_public.json
+
+  depends_on = [aws_s3_bucket_public_access_block.main]
+}
+
+data "aws_iam_policy_document" "allow_content_public" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.main.arn}/*",
+    ]
+  }
+}
+
+# Synchronisation des fichiers
+resource "aws_s3_object" "sync_remote_website_content" {
+  for_each = fileset(var.sync_directories[0].local_source_directory, "**/*.*")
+
+  bucket = aws_s3_bucket.main.id
+  key    = "${var.sync_directories[0].s3_target_directory}/${each.value}"
+  source = "${var.sync_directories[0].local_source_directory}/${each.value}"
+  etag   = filemd5("${var.sync_directories[0].local_source_directory}/${each.value}")
+  content_type = try(
+    lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1]),
+    "binary/octet-stream"
+  )
 }
 
 # Distribution CloudFront
 resource "aws_cloudfront_distribution" "main" {
-  enabled             = true
-  default_root_object = "index.html"
-  comment             = "Distribution for ${var.bucket_name}"
-
   origin {
-    domain_name              = aws_s3_bucket.main.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
-    origin_id                = "S3-${aws_s3_bucket.main.id}"
+    domain_name = aws_s3_bucket_website_configuration.main.website_endpoint
+    origin_id   = "S3-${aws_s3_bucket.main.bucket}"
+    
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
 
   default_cache_behavior {
     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-${aws_s3_bucket.main.id}"
+    target_origin_id       = "S3-${aws_s3_bucket.main.bucket}"
     compress               = true
-    viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       query_string = false
@@ -67,12 +99,12 @@ resource "aws_cloudfront_distribution" "main" {
       }
     }
 
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
-  # Gestion des erreurs SPA (React Router)
   custom_error_response {
     error_code         = 403
     response_code      = 200
@@ -96,46 +128,6 @@ resource "aws_cloudfront_distribution" "main" {
   }
 
   tags = var.tags
-}
-
-# Politique S3 pour CloudFront uniquement
-data "aws_iam_policy_document" "allow_cloudfront" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "${aws_s3_bucket.main.arn}/*",
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.main.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "allow_cloudfront" {
-  depends_on = [aws_s3_bucket_public_access_block.main]
-  bucket     = aws_s3_bucket.main.id
-  policy     = data.aws_iam_policy_document.allow_cloudfront.json
-}
-
-resource "aws_s3_object" "sync_remote_website_content" {
-  for_each = fileset(var.sync_directories[0].local_source_directory, "**/*.*")
-
-  bucket = aws_s3_bucket.main.id
-  key    = "${var.sync_directories[0].s3_target_directory}/${each.value}"
-  source = "${var.sync_directories[0].local_source_directory}/${each.value}"
-  etag   = filemd5("${var.sync_directories[0].local_source_directory}/${each.value}")
-  content_type = try(
-    lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1]),
-    "binary/octet-stream"
-  )
 }
 
 resource "random_integer" "random" {
